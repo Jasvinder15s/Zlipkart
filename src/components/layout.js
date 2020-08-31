e2e62091c3041ee6837e4d46c18809ef4c97438c
Jasvinder Singh <jvsinghk@gmail.com>
2020-08-31 22:14:04 +0530

    created footer component in react

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,27 +8,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Footer from "./Reuseable/Footer"
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Layout = ({children}) => (
+  <div>
+    {children}
+    <Footer />
+  </div>
+)
 
-  return (
-    <div>
-      <main>{children}</main>
-    </div>
-  )
-}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
